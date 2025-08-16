3ac6fb5e3f9374fdc82bdc562f78ac551d9e7440
moraissdev <renanbusinessdev@gmail.com>
2025-08-16 06:05:34 -0300

    commit

---
+++ Curso de Python/Exercícios/exercicio15.py
@@ -0,0 +1,4 @@
+qtd_dias = int(input("Quantos dias alugado: "))
+qtd_km = float(input("Quantos km rodados: "))
+total = (qtd_dias * 60) + (0.15 * qtd_km)
+print("Sabendo que voce alugou o carro por {} dias e percorreu {} km, o total será de R${:.2f}".format(qtd_dias, qtd_km, total))
