ba1e6a6d20ade7f0c85c5424eaeb85309ce94824
moraissdev <renanbusinessdev@gmail.com>
2025-09-01 12:19:27 -0300

    commit

--- Aulas - UNIFEI/Aula 03/ex10.py
+++ Aulas - UNIFEI/Aula 03/ex10.py
@@ -1,11 +1,29 @@
-def converte(hrs, min, seg): #converte horas, minutos e segundos para segundos.
-    return (hrs * 3600) + (min * 60) + seg 
+def converte_seg(horas, minutos, segundos):
+    seg_total = (horas * 3600) + (minutos * 60) + segundos
+    return seg_total
 
-hora = int(input("Horas: "))
-minuto = int(input("Minutos: "))
-segundo = int(input("Segundos: "))
+def converte_hrs_min_seg(segundos_total):
+    horas = (segundos_total // 3600)
+    minutos = ((segundos_total - (horas * 3600)) // 60)
+    segundos = segundos_total - ((horas * 3600) + (minutos * 60))
+    print('O usuário ficou {} h {} min {} seg conectado na internet'.format(horas, minutos, segundos))
 
-#chamando a função
-total_segundo = converte(hora, minuto, segundo)
+# quando o usuário entrou na internet
+print("Entrada na internet:")
+horas_entrada = int(input("Hora de entrada: "))
+minutos_entrada = int(input("Minuto de entrada: "))
+segundos_entrada = int(input("Segundo de entrada: "))
 
-print("{} hora(s), {} minuto(s) e {} segundo(s) = {} segundos".format(hora, minuto, segundo, total_segundo))
+# quando o usuário saiu da internet
+print("\nSaída da internet:")
+horas_saida = int(input("Hora de saída: "))
+minutos_saida = int(input("Minuto de saída: "))
+segundos_saida  = int(input("Segundo de saída: "))
+
+#desconsidere a hipótese de que a desconexão tenha ocorrido após a meia-noite.
+
+seg_total_entrada = converte_seg(horas_entrada, minutos_entrada, segundos_entrada) #converter os dados de entrada para segundos
+seg_total_saida = converte_seg(horas_saida, minutos_saida, segundos_saida) #converter os dados de saída para segundos
+
+seg_total = seg_total_saida - seg_total_entrada
+converte_hrs_min_seg(seg_total)
--- Aulas - UNIFEI/Aula 03/ex11.py
+++
@@ -1,9 +0,0 @@
-def converte(total_segundo): #converte segundos para horas, minutos e segundos.
-    hora = (total_segundo // 3600)
-    minuto = (total_segundo - (hora * 3600)) // 60
-    segundo = (total_segundo - (hora * 3600 + minuto * 60))
-    print("{} segundo(s) = {} hora(s), {} minuto(s) e {} segundo(s)".format(total_segundo, hora, minuto, segundo))
-
-segundo = int(input("Total de segundos: "))
-#chamando a função
-converte(segundo)
