c86b549c71f56e9b62ee955e3c08d205a70fafa2
moraissdev <renanbusinessdev@gmail.com>
2025-10-20 02:27:58 -0300

    commit

--- Aulas - UNIFEI/POO/Trabalhos/trabalho07.py
+++ Aulas - UNIFEI/POO/Trabalhos/trabalho07.py
@@ -111,8 +111,8 @@ if __name__ == "__main__":
     funcComissionado.adicionaVenda(200, 3, 2022, 200000) 
     funcComissionado.adicionaVenda(201, 3, 2022, 400000) 
     funcComissionado.adicionaVenda(202, 4, 2022, 500000) 
-    listaFuncionarios = [funcContratado, funcComissionado] 
-    for func in listaFuncionarios: 
+    listaFunc = [funcContratado, funcComissionado] 
+    for func in listaFunc: 
         print (func.getDados()) 
         print ("Renda no mês 3 de 2022: ") 
         print (func.calculaRenda(3, 2022))
